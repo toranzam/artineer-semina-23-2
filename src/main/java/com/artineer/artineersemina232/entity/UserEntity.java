3447e8e68fd56c22d1c33eb11808f22d50c27e2f
Dangmeon <dlekdus9320@gmail.com>
2023-10-31 16:23:49 +0900

    ADD

---
+++ src/main/java/com/artineer/artineersemina232/entity/UserEntity.java
@@ -0,0 +1,26 @@
+package com.artineer.artineersemina232.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String username;
+
+    @Column(unique = true)
+    private String password;
+}
