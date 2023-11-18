1e534babca0e5c9027da62b7a9a55b061461711e
toranzam <nbc05829@gmail.com>
2023-11-18 19:45:53 +0900

    ADD profile FIX form css

--- src/main/java/com/artineer/artineersemina232/controller/AccountController.java
+++ src/main/java/com/artineer/artineersemina232/controller/AccountController.java
@@ -1,7 +1,10 @@
 package com.artineer.artineersemina232.controller;
 
 
+import com.artineer.artineersemina232.auth.CurrentUser;
 import com.artineer.artineersemina232.dto.AccountDto;
+import com.artineer.artineersemina232.entity.Account;
+import com.artineer.artineersemina232.repository.AccountRepository;
 import com.artineer.artineersemina232.service.AccountService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -10,6 +13,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -18,6 +22,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 public class AccountController {
 
     private final AccountService accountService;
+
+    private final AccountRepository accountRepository;
 
     @GetMapping("/loginPage")
     public String loginPage() {
@@ -42,5 +48,21 @@ public class AccountController {
         // 자동로그인
 //        accountService.login(userDto.getUsername());
         return "redirect:/loginPage"; //요청된 장소로 가야함 Form/ 쓰면 안됨. 써서 없는 페이지로 가고 있었음.
+    }
+
+    @GetMapping("/profile/{username}")
+    public String showProfile(@PathVariable String username, Model model, @CurrentUser Account account) {
+
+        Account byUsername = accountRepository.findByUsername(username);
+        if(byUsername == null){
+            throw new IllegalArgumentException(username + "에 해당하는 사용자가 존재하지않습니다");
+        }
+
+
+        model.addAttribute("account", byUsername);
+        model.addAttribute("isOwner", account.equals(byUsername));
+
+
+        return "account/profile";
     }
 }
